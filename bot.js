a9b22dbfe5af0a2d5f25f0ca5fbd577620defb66
projsh <projsh@users.noreply.github.com>
2017-09-03 20:52:58 +1000

    general fixes (2.0.1)

--- bot.js
+++ bot.js
@@ -16,12 +16,14 @@
  * This project's repository: https://github.com/projsh/Cerium
  * 
  * *************************************/
-
+ 
 const Discord = require("discord.js");
 const config = require("./config.json")
 const ytdl = require("ytdl-core");
 const chalk = require("chalk");
 const fs = require("fs");
+const blessed = require("blessed");
+const readline = require("readline");
 const bot = new Discord.Client();
 const cver = config.ver;
 
@@ -34,9 +36,51 @@ var guildspeak = null;
 var channspeak = null;
 bot.commands = new Discord.Collection();
 
+//console
+var screen = blessed.screen({
+    smartCSR: true,
+    dockBorders: true
+});
+screen.title = 'Cerium ' + cver;
+
+var titleBox = blessed.text({
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "1",
+    content: "Cerium " + cver + " Console",
+    tags: true,
+    style: {
+        fg: 'black',
+        bg: 'white'
+    },
+    padding: {
+        left: 1
+    }
+});
+screen.append(titleBox);
+
+var stdinInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+stdinInterface.on("line", function(line) {
+    //Input received!
+	
+    var lLine = line.toLowerCase();
+    if (lLine == "help") {
+        console.log(chalk.yellow("\n[Cerium Console Commands]") + "\nConsole commands are coming soon!");
+    } else {
+        console.log(chalk.yellow("[CONSOLE] Console commands are coming soon!"));
+    }
+});
+
 //When bot is ready
 bot.on('ready', () => {
     console.log(chalk.green("[INFO] Success! I've logged into the following bot account:", chalk.blue(bot.user.username)));
+    console.log(chalk.green("[INFO] Welcome to Cerium " + chalk.blue("v." + cver) + "\n[INFO] Current prefix is:", chalk.blue(prefix)));
     bot.setInterval(setGame, 300000);
     setGame();
     if (config.embedcolor.length == 0 || config.embedcolor.length >= 8) {
@@ -50,8 +94,6 @@ bot.on('ready', () => {
     if (hostid.length == 0) {
         console.log(chalk.red("[ERROR] Cannot find the host's user ID in config.json. Cannot continue operation."));
         process.exit;
-    } else {
-        console.log(chalk.green("[INFO] The host's user ID is: " + hostid));
     }
 });
 
@@ -74,25 +116,25 @@ function setGame() {
             presence.game.name = "a game";
             break;
         case 1:
-            presence.game.name = "all night long";
+            presence.game.name = "a youtube video";
             break;
         case 2:
             presence.game.name = "un-breaking";
             break;
         case 3:
-            presence.game.name = "trying to defeat astralmod and jxbot";
+            presence.game.name = "something";
             break;
         case 4:
             presence.game.name = "lel";
             break;
         case 5:
-            presence.game.name = "uhh nothing...";
+            presence.game.name = prefix + "help for help";
             break;
         case 6:
-            presence.game.name = "not breaking down during the night";
+            presence.game.name = "Wow Ethan! Great moves, keep it up! Proud of you!";
             break;
         case 7:
-            presence.game.name = "i can stay online for long periods of time ;)";
+            presence.game.name = "wew";
             break;
         case 8:
             presence.game.name = "v." + cver;
@@ -123,16 +165,24 @@ fs.readdir("./modules/", (err, files) => {
     console.log(chalk.cyan("[MODULES] Loaded " + modules.length + " modules successfully."));
 })
 
+//login
+bot.login(config.token).catch(function() {
+    console.log(chalk.red("[ERROR] Failed to login. Exiting..."));
+    process.exit();
+});
+
 bot.on("message", async message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
+    var msg = message.content.substr(config.prefix.length);
 
     let array = message.content.split(" ");
     let command = array[0];
     let args  = array.slice(1);
 
     if (!command.startsWith(config.prefix)) return;
-
+    if (msg.includes(config.prefix)) return;
+    if (!msg) return;
     let cmd = bot.commands.get(command.slice(config.prefix.length))
 
     if (cmd) {
@@ -140,10 +190,4 @@ bot.on("message", async message => {
     } else {
         message.channel.send("**Error:** Command not found. Type `" + config.prefix + "help` for a list of valid commands.");
     }
-});
-
-console.log(chalk.green("[INFO] Welcome to Cerium " + chalk.blue("v." + cver) + "\n[INFO] Reading config.json and logging in...\n[INFO] Make sure Cerium has full access to each server.\n[INFO] Current prefix is:", chalk.blue(prefix)));
-bot.login(config.token).catch(function() {
-    console.log(chalk.red("[ERROR] Failed to login. Exiting..."));
-    process.exit();
 });
--- modules/ship.js
+++ modules/ship.js
@@ -1,7 +1,25 @@
 module.exports.run = async (bot, message, args) => {
+    try {
+    const config = require("../config.json");
+    var msg = message.content.substr(5);
     var shipuser = message.guild.members.random().displayName
-    var shipname = message.author.username.substring(0,3) + shipuser.substr(3);
-    message.channel.send(":ship: " + message.author.username + " x " + shipuser + " (Ship name: " + shipname + ")");
+    var shipnameauth = message.author.username.substring(0,3) + shipuser.substring(0,3);
+    if (!args[0]) {
+        message.channel.send(":ship: " + message.author.username + " x " + shipuser + " (" + shipnameauth + ")");
+    } else if (msg.includes("@")) {
+        var findm = msg.replace("<", "").replace(">", "").replace("@", "").replace("!", "").replace(/[^0-9.]/g, "");
+        var member = message.guild.members.get(findm);
+        var shipnamemem = member.user.username.substring(0,3) + shipuser.substring(0,3);    
+        message.channel.send(":ship: " + member.user.username + " x " + shipuser + " (" + shipnamemem + ")");
+    } else {
+        var shipnamereg = args[0].substring(0,3) + shipuser.substring(0,3);
+        message.channel.send(":ship: " + args + " x " + shipuser + " (" + shipnamereg + ")");
+    }
+} catch(error){
+    message.channel.send("**Error:** Failed to ship you.")
+    console.error(error)
+}
+
 }
 module.exports.help = {
     name: "ship",
