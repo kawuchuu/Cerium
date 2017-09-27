77bb2bb21f9ca106214d57b65d803017bba0bfb3
projsh <projsh@users.noreply.github.com>
2017-09-28 00:30:47 +1000

    avatar embedded

--- modules/avatar.js
+++ modules/avatar.js
@@ -1,13 +1,20 @@
-module.exports.run = async (bot, message, args, config) => {
+module.exports.run = async (bot, message, args, Discord) => {
     var config = require('../config.json');
     var msg = message.content.substr(config.prefix.length + 6);
+    embed = new Discord.RichEmbed();
+    embed.setColor(config.embedcolor);
+    embed.setFooter("Cerium v." + config.ver);
     if (!msg) {
-        message.channel.send(message.author.displayAvatarURL);
+        embed.setImage(message.author.displayAvatarURL);
+        embed.setAuthor(message.author.username + "'s Avatar");
+        message.channel.send({embed: embed});
     } else {
         try {
             var findm = msg.replace("<", "").replace(">", "").replace("@", "").replace("!", "").replace(/[^0-9.]/g, "");
             var member = message.guild.members.get(findm);
-            message.channel.send(member.user.displayAvatarURL);
+            embed.setAuthor(member.user.username + "'s Avatar");
+            embed.setImage(member.user.displayAvatarURL);
+            message.channel.send({embed: embed});
         } catch(error) {
             message.channel.send("**Error:** Cannot find that user.");
         }
