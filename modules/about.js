295beb1e921b538a4262ce7f59ff25073b1de40e
Joshua Walker <projsh@users.noreply.github.com>
2018-01-17 16:27:00 +1100

    added cerium's website and discord bot list page

--- modules/about.js
+++ modules/about.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args, Discord, config, cver) => {
     embed.setColor(config.embedcolor);
     embed.addField("General Information","Cerium is a Discord bot created by Joshua Walker (projsh_)\nIt's a free & open-source project available on GitHub.", true);
     embed.addField("Bot Information", `**Version** - v.${config.ver}\n**Account** - ${bot.user.tag}\n**Library** - [discord.js](https://discord.js.org)\n**Runtime** - [Node.js](https://nodejs.org)`, true);
-    embed.addField("Links", "[GitHub Repo](https://www.github.com/projsh/Cerium)\n[Report Bugs](https://www.github.com/projsh/Cerium/issues)\n[README File](https://github.com/projsh/Cerium/blob/master/README.md)\n[License](https://www.github.com/projsh/Cerium/blob/master/LICENSE)", true);
+    embed.addField("Links", "[Website](https://projsh.github.io/cerium)\n[Discord Bot List](https://discordbots.org/bot/300513436675080205)\n[GitHub Repo](https://www.github.com/projsh/Cerium)\n[Report Bugs](https://www.github.com/projsh/Cerium/issues)\n[README File](https://github.com/projsh/Cerium/blob/master/README.md)\n[License](https://www.github.com/projsh/Cerium/blob/master/LICENSE)\n", true);
 
     embed.setFooter("<> with <3 by projsh_");
     message.channel.send({embed: embed});
